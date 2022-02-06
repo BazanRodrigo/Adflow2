a7aca743692f767b04f042964082d59720b207ec
BazanRodrigo <bazanzuritarodrigo@gmail.com>
2022-02-05 18:09:15 -0600

    insercion de configuracion yml

--- app.py
+++ app.py
@@ -209,6 +209,6 @@ def main():
     # start flask app
     #     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=4000, ssl_context="adhoc")
+    app.run(host='127.0.0.1', debug=True, port=8080, ssl_context="adhoc")
     #app.run(debug=False)
 #https://medium.com/analytics-vidhya/object-detection-using-yolo-v3-and-deploying-it-on-docker-and-minikube-c1192e81ae7a
